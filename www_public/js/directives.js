740845e01985c1aca1ccd36e089831f67e5e0d92
Christian Ramseyer <ramseyer@netnea.com>
2026-06-24 19:06:29 +0000

    fix weather widget angular handling

--- www_public/js/directives.js
+++ www_public/js/directives.js
@@ -9,19 +9,44 @@ angular.module('myApp.directives', []).
       elm.text(version);
     };
   }]).
-  directive('srfWeatherWidget', [function() {
+  directive('srfWeatherWidgetLoader', ['$timeout', function($timeout) {
     return {
       restrict: 'C',
-      link: function(scope, elm) {
+      link: function(scope, elm, attrs) {
         var src = 'https://mmz-srf.github.io/srf-weather-widget/main.js';
-        if (!document.querySelector('script[data-srf-weather-widget-script]')) {
+        var lastLocationName;
+
+        function loadWidget(locationName) {
+          if (!locationName || locationName === lastLocationName) {
+            return;
+          }
+
+          lastLocationName = locationName;
+          elm.empty();
+
+          var target = document.createElement('div');
+          target.className = 'srf-weather-widget';
+          target.setAttribute('data-size', attrs.size || 'S');
+          target.setAttribute('data-location-name', locationName);
+          elm.append(target);
+
+          // The SRF module initializes matching nodes once when it is evaluated.
+          // Load a fresh module URL for each Angular-created widget target.
           var script = document.createElement('script');
           script.async = true;
           script.type = 'module';
-          script.src = src;
-          script.setAttribute('data-srf-weather-widget-script', '1');
+          script.src = src + '?location=' + encodeURIComponent(locationName) + '&t=' + Date.now();
+          script.onload = function() {
+            target.className = '';
+          };
           document.body.appendChild(script);
         }
+
+        attrs.$observe('locationName', function(locationName) {
+          $timeout(function() {
+            loadWidget(locationName);
+          });
+        });
       }
     };
   }]);
